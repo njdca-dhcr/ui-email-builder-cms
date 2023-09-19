99600aeef93e2fe5ce7c2104bc5727448aab8088
Michael Crismali <michael@crismali.com>
2023-09-19 10:23:03 -0500

    Added Reach skip nav component

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -1,6 +1,8 @@
 import React, { FC, ReactNode } from 'react'
-import { Navigation } from './Navigation'
 import { Link } from 'gatsby'
+import { SkipNavLink, SkipNavContent } from '@reach/skip-nav'
+import '@reach/skip-nav/styles.css'
+import { Navigation } from './Navigation'
 
 interface Props {
   children: ReactNode
@@ -11,10 +13,12 @@ export const TEST_ID = 'layout'
 export const Layout: FC<Props> = ({ children }) => {
   return (
     <div data-testid={TEST_ID}>
+      <SkipNavLink />
       <div style={styles.header}>
         <Link to="/">Email Builder</Link>
         <Navigation />
       </div>
+      <SkipNavContent />
       {children}
     </div>
   )
