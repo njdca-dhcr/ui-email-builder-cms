899669cfea87d7e1e4a8bbaf3217e5a4e9dc2680
Michael Crismali <michael@crismali.com>
2023-09-19 16:17:57 -0500

    Added doctype to the rendered email

--- src/templates/emailForm/__tests__/useElementsToEmailString.test.tsx
+++ src/templates/emailForm/__tests__/useElementsToEmailString.test.tsx
@@ -48,5 +48,11 @@ describe('useElementsToEmailString', () => {
       expect(resultCallback()).toContain(text)
       expect(resultCallback()).toContain('<span class="foo"')
     })
+
+    it('creates email markup with a doctype', () => {
+      expect(resultCallback()).toContain(
+        `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">`,
+      )
+    })
   })
 })
--- src/templates/emailForm/useElementsToEmailString.tsx
+++ src/templates/emailForm/useElementsToEmailString.tsx
@@ -2,13 +2,18 @@ import React, { MutableRefObject, useCallback } from 'react'
 import { renderToString } from 'react-dom/server'
 import { EmailLayout } from './EmailLayout'
 
+const DOCTYPE = `<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">`
+
 export const useElementsToEmailString = (
   ref: MutableRefObject<HTMLElement | undefined>,
   title: string,
 ): (() => string) => {
   return useCallback(() => {
     if (ref.current) {
-      return renderToString(<EmailLayout html={ref.current.innerHTML} title={title}></EmailLayout>)
+      const rendered = renderToString(
+        <EmailLayout html={ref.current.innerHTML} title={title}></EmailLayout>,
+      )
+      return `${DOCTYPE}${rendered}`
     } else {
       return ''
     }
