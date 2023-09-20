bcc09bf2f07ee0edd4dfd322c3d91a70a951bf81
Michael Crismali <michael@crismali.com>
2023-09-20 10:09:05 -0500

    Refactored some of the default gatsby styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,17 @@
 import React, { FC } from 'react'
 import type { HeadFC } from 'gatsby'
 import { Layout } from '../ui/Layout'
-import './index.css'
 
 const IndexPage: FC = () => {
   return (
     <Layout>
-      <main className="page">
-        <h1 className="heading">Email Builder CMS</h1>
-        <p className="paragraph">This is a placeholder homepage for now.</p>
-        <p className="paragraph">
-          <a href="/admin" className="link">
-            The admin interface can be accessed here.
-          </a>
-        </p>
-      </main>
+      <h1 className="heading">Email Builder CMS</h1>
+      <p className="paragraph">This is a placeholder homepage for now.</p>
+      <p className="paragraph">
+        <a href="/admin" className="link">
+          The admin interface can be accessed here.
+        </a>
+      </p>
     </Layout>
   )
 }
--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -13,14 +13,14 @@ export const TEST_ID = 'layout'
 
 export const Layout: FC<Props> = ({ children }) => {
   return (
-    <div data-testid={TEST_ID}>
+    <div data-testid={TEST_ID} className="layout">
       <SkipNavLink />
       <div className="header">
         <Link to="/">Email Builder</Link>
         <Navigation />
       </div>
       <SkipNavContent />
-      {children}
+      <main className="page">{children}</main>
     </div>
   )
 }
