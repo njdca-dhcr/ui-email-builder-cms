7f53c49571ecb847bbc5a161aebaddd708c16887
Michael Crismali <michael@crismali.com>
2023-09-27 10:41:42 -0500

    use a bare png instead of an svg in emails

--- src/ui/WarningIcon.tsx
+++ src/ui/WarningIcon.tsx
@@ -1,21 +1,5 @@
 import React, { FC } from 'react'
 
 export const WarningIcon: FC = () => {
-  return (
-    <svg
-      aria-hidden
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M1.3335 27.9998H30.6668L16.0002 2.6665L1.3335 27.9998ZM17.3335 23.9998H14.6668V21.3332H17.3335V23.9998ZM17.3335 18.6665H14.6668V13.3332H17.3335V18.6665Z"
-        fill="#1B1B1B"
-      />
-    </svg>
-  )
+  return <img src="/warning-icon.png" alt="" />
 }
