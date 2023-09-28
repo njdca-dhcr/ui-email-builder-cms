e228afd7043a7e5b502908a1aa4dcd81d345a688
Michael Crismali <michael@crismali.com>
2023-09-28 15:39:20 -0500

    Added some basic styles for AdditionalContent

--- src/templates/EmailTemplateSubComponents/AdditionalContent.tsx
+++ src/templates/EmailTemplateSubComponents/AdditionalContent.tsx
@@ -2,7 +2,7 @@ import React, { CSSProperties, FC } from 'react'
 import { EmailSubComponentProps } from './shared'
 import { useIsCurrentlyActiveEmailSubComponent } from '../CurrentlyActiveEmailPart'
 import { useEmailPartsContentForSubComponent } from '../EmailPartsContent'
-import { Colors, Font } from '../styles'
+import { DefaultStyles } from '../styles'
 import { EditableElement } from 'src/ui/EditableElement'
 
 export const AdditionalContent: FC<EmailSubComponentProps> = ({ id, componentId }) => {
@@ -25,4 +25,7 @@ export const AdditionalContent: FC<EmailSubComponentProps> = ({ id, componentId 
   )
 }
 
-const styles: CSSProperties = {}
+const styles: CSSProperties = {
+  ...DefaultStyles,
+  paddingBottom: 20,
+}
