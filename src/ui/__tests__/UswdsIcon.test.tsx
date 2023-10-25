56689083d73b52978d7b04501518bf0ea8216e95
Eryan Cobham <eryan@cobwebsconsulting.com>
2023-10-25 09:46:46 -0500

    fix test

--- src/ui/__tests__/UswdsIcon.test.tsx
+++ src/ui/__tests__/UswdsIcon.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { UswdsIcon } from '../UswdsIcon'
-import { urlFor } from 'src/testHelpers'
 
 describe('UswdsIcon', () => {
   it('displays the icon for the given key', () => {
@@ -9,6 +8,6 @@ describe('UswdsIcon', () => {
     const img: HTMLImageElement | null = queryByRole('img') as any
     expect(img).not.toBeNull()
     expect(img?.alt).toEqual('')
-    expect(img?.src).toEqual(urlFor('/icons/account_balance.png'))
+    expect(img?.src).toMatch(/account_balance\.png/)
   })
 })
