17f65211e4cd7e458e7b541dff62013fb8e20e47
Michael Crismali <michael@crismali.com>
2023-11-10 10:26:46 -0600

    Improved the accessibility of the preview text character count

--- src/templates/EmailEditorSidebar/EditPreviewText.tsx
+++ src/templates/EmailEditorSidebar/EditPreviewText.tsx
@@ -12,7 +12,9 @@ export const EditPreviewText: FC = () => {
     <SpacedSidebarContainer className="edit-preview-text">
       <div className="edit-preview-text-content">
         <h2>Preview Text</h2>
-        <p className="description">1-2 sentences that will preview the content of your email</p>
+        <p id="preview-text-description" className="description">
+          1-2 sentences that will preview the content of your email
+        </p>
         <div className="subject-and-preview">
           <p className="subject">[Subject Line] Don't forget this on your platform!</p>
           <VisuallyHidden>
@@ -23,9 +25,10 @@ export const EditPreviewText: FC = () => {
             value={value}
             onChange={(event) => setValue(event.target.value)}
             placeholder="This is the preview text that you can edit. It gives insight into the email so that people will open it."
+            aria-describedby="preview-text-description preview-text-character-count"
           />
         </div>
-        <div className="suggestion-and-count">
+        <div id="preview-text-character-count" className="suggestion-and-count">
           <p>80-120 characters is ideal</p>
           <p>
             {value.length}
