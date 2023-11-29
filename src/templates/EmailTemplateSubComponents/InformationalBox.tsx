2965a3e54bc1077f1747d5aa131254862a06f005
Eryan Cobham <eryan@cobwebsconsulting.com>
2023-11-29 12:54:34 -0600

    basic structure of the box

--- src/templates/EmailTemplateSubComponents/InformationalBox.tsx
+++ src/templates/EmailTemplateSubComponents/InformationalBox.tsx
@@ -1,26 +1,87 @@
-import React, { FC } from 'react'
+import React, { CSSProperties, FC } from 'react'
 import { EmailSubComponentProps } from '../EmailTemplateSubComponents/shared'
 import { EmailBlock } from 'src/ui/EmailBlock'
 import { EditableElement } from 'src/ui/EditableElement'
 import { useIsCurrentlyActiveEmailPart } from '../CurrentlyActiveEmailPart'
+import { useEmailPartsContentFor } from '../EmailPartsContent'
+import { Borders, Spacing, SpacingCell, StyleDefaults, Text, Font } from '../styles'
 import { BoxColor, BoxColorConfigs } from 'src/ui/SelectBoxColor'
+import { UswdsIcon, UswdsIconVariantKey } from 'src/ui/UswdsIcon'
 
-
-const { Row, Cell, Link } = EmailBlock
-
-interface LoginDetailsValue {
-  loginInformationIcon: UswdsIconVariantKey
+interface InformationalBoxValue {
+  boxColor: BoxColor
+  icon: UswdsIconVariantKey
+  title: string
+  description: string
 }
 
-const defaultValue: LoginDetailsValue = {
-  loginInformationIcon: 'LockOpen',
+const defaultValue: InformationalBoxValue = {
+  boxColor: BoxColor.BenefitBlue,
+  icon: 'LockOpen',
+  title: 'Application confirmation number',
+  description: 'Confirmation number: 123456789',
 }
+
+export const useInformationalBoxValue = (id: string) => {
+  return useEmailPartsContentFor(id, defaultValue)
+}
+
+const { Table, Row, Cell } = EmailBlock
 
 export const InformationalBox: FC<EmailSubComponentProps> = ({ emailSubComponent }) => {
   const { activate } = useIsCurrentlyActiveEmailPart(emailSubComponent.id)
+  const [value, setValue] = useInformationalBoxValue(emailSubComponent.id)
+
+  const boxColorConfig = BoxColorConfigs[value.boxColor]
+
   return (
-    <div>
-      <h1>InformationalBox</h1>
-    </div>
+    <>
+      <Row
+        onClick={activate}
+        elements={[
+          'cell',
+          'table',
+          'row',
+          { part: 'cell', style: styles.outerCell, className: StyleDefaults.layout.wide },
+          'table',
+          'row',
+          {
+            part: 'cell',
+            style: {
+              ...styles.innerCell,
+              backgroundColor: boxColorConfig.backgroundColor,
+              borderLeft: Borders.large(boxColorConfig.accentColor),
+            },
+          },
+          'table',
+          'row',
+        ]}
+      >
+        <Cell>
+          <UswdsIcon icon={value.icon} />
+        </Cell>
+        <Cell elements={['table']}>
+          <Row>
+            <Cell>value.title</Cell>
+          </Row>
+          <Row>
+            <Cell>value.description</Cell>
+          </Row>
+        </Cell>
+      </Row>
+      <Row>
+        <SpacingCell size='extraLarge'/>
+      </Row>
+    </>
   )
+}
+
+const styles = {
+  outerCell: {
+    ...StyleDefaults.inline.colors,
+  } as CSSProperties,
+  innerCell: {
+    paddingTop: Spacing.size.medium,
+    paddingBottom: Spacing.size.medium,
+  } as CSSProperties,
 }
