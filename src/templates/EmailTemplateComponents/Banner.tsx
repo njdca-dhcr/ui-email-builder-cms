c26f50a036f436db69887452209b58efe815b4a1
Eryan Cobham <eryan@cobwebsconsulting.com>
2024-02-05 14:34:04 -0600

    fixes for the banner

--- src/templates/EmailTemplateComponents/Banner.tsx
+++ src/templates/EmailTemplateComponents/Banner.tsx
@@ -47,7 +47,7 @@ export const BannerMarkup: FC<BannerMarkupProps> = ({
           {primaryText}
         </Comp>
       </Cell>
-      <Cell className="banner-link-container" style={secondaryLinkContainerStyles}>
+      <Cell className="banner-link-container top-nav-second-link" style={secondaryLinkContainerStyles}>
         <Comp to={secondaryLink} style={{ ...secondaryLinkStyles, color }}>
           {getHostName(secondaryLink)}
         </Comp>
