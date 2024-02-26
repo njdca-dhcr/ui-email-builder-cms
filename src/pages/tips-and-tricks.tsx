d5551f064bd0faabc2955176f441e8dc54b4bda6
Michael Crismali <michael@crismali.com>
2024-02-26 14:28:54 -0600

    Removed additional scaffolding

--- src/pages/tips-and-tricks.tsx
+++ src/pages/tips-and-tricks.tsx
@@ -1,33 +1,11 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import { HeadFC } from 'gatsby'
 import { SkipNavContent } from '@reach/skip-nav'
 import { Heading, Layout, PageContent, Paragraph, Sidebar, SpacedContainer } from 'src/ui/Layout'
 import { SidebarNavigation } from 'src/ui/SidebarNavigation'
 import { formatPageTitle } from 'src/utils/formatPageTitle'
-import { RichTextValue } from 'src/ui/RichTextEditor'
 
 const TipsAndTricksPage: FC = () => {
-  const [value, setValue] = useState<RichTextValue>([
-    {
-      type: 'paragraph',
-      children: [
-        { text: 'This is editable ' },
-        { text: 'rich', bold: true },
-        { text: ' text, ' },
-        { text: 'much', italic: true },
-        { text: ' better than a ' },
-        { text: '<textarea>' },
-        { text: '! ' },
-        { text: 'Also there is a ' },
-        {
-          type: 'link',
-          url: 'https://en.wikipedia.org/wiki/Hypertext',
-          children: [{ text: 'hyperlink' }],
-        },
-      ],
-    },
-  ])
-
   return (
     <Layout element="div">
       <Sidebar>
