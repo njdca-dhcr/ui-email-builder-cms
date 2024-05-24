2da0674a8ca31e0aaf26860f42be3ea8da30115d
Michael Crismali <michael@crismali.com>
2024-05-24 10:34:54 -0500

    Added useCreateOrUpdateEmailTemplate

---
+++ src/network/useCreateOrUpdateEmailTemplate.ts
@@ -0,0 +1,20 @@
+import { useCreateEmailTemplate } from './useCreateEmailTemplate'
+import { useUpdateEmailTemplate } from './useUpdateEmailTemplate'
+
+export function useCreateOrUpdateEmailTemplate(
+  id: undefined,
+): ReturnType<typeof useCreateEmailTemplate>
+export function useCreateOrUpdateEmailTemplate(
+  id: string,
+): ReturnType<typeof useUpdateEmailTemplate>
+export function useCreateOrUpdateEmailTemplate(id: string | undefined) {
+  const update = useUpdateEmailTemplate(`${id}`)
+  const create = useCreateEmailTemplate()
+
+  switch (typeof id) {
+    case 'string':
+      return update
+    default:
+      return create
+  }
+}
