3870c76bd8024561884a651fc2718a7824fa5ae1
Michael Crismali <michael@crismali.com>
2024-12-18 12:15:57 -0600

    Styled TranslationLinks

--- src/templates/EmailTemplateComponents/TranslationLinks.tsx
+++ src/templates/EmailTemplateComponents/TranslationLinks.tsx
@@ -1,12 +1,13 @@
-import React, { FC } from 'react'
+import React, { CSSProperties, FC } from 'react'
 import { EmailComponentProps } from './shared'
 import { EditableElement, EmailBlock } from 'src/ui'
 import { useEmailPartsContentFor } from '../EmailPartsContent'
 import { useCurrentEmailTemplate, useCurrentLanguage } from 'src/utils/EmailTemplateState'
 import capitalize from 'lodash.capitalize'
 import { useIsCurrentlyActiveEmailComponent } from '../CurrentlyActiveEmailPart'
+import { Colors, Font, Spacing, StyleDefaults, Text } from '../styles'
 
-const { Row, Cell, Link } = EmailBlock
+const { Row, Link } = EmailBlock
 
 export const TranslationLinks: FC<EmailComponentProps<'TranslationLinks'>> = ({
   emailComponent,
@@ -19,45 +20,73 @@ export const TranslationLinks: FC<EmailComponentProps<'TranslationLinks'>> = ({
 
   return (
     <Row
-      className="TranslationLinks"
+      className="translation-links"
       onClick={(event) => {
         activate(event)
       }}
+      elements={[
+        { part: 'cell', style: styles.outerCell },
+        'table',
+        'row',
+        { part: 'cell', align: 'center' },
+      ]}
     >
-      <Cell>
-        <EditableElement
-          element="span"
-          label={`${capitalize(language)} label`}
-          onValueChange={(text) => {
-            setValue({
-              languages: { ...value.languages, [language]: { ...value.languages[language], text } },
-            })
-          }}
-          value={value.languages[language]?.text ?? ''}
-        />
-      </Cell>
-
       {translations.map((translation) =>
-        translation.language === language ? null : (
-          <Cell key={translation.language}>
-            <Link to={value.languages[translation.language]?.href ?? ''}>
-              <EditableElement
-                element="span"
-                label={`${capitalize(translation.language)} label`}
-                onValueChange={(text) => {
-                  setValue({
-                    languages: {
-                      ...value.languages,
-                      [translation.language]: { ...value.languages[translation.language], text },
-                    },
-                  })
-                }}
-                value={value.languages[translation.language]?.text ?? ''}
-              />
-            </Link>
-          </Cell>
+        translation.language === language ? (
+          <EditableElement
+            key={translation.language}
+            element="span"
+            style={styles.currentLanguage}
+            label={`${capitalize(language)} label`}
+            onValueChange={(text) => {
+              setValue({
+                languages: {
+                  ...value.languages,
+                  [language]: { ...value.languages[language], text },
+                },
+              })
+            }}
+            value={value.languages[language]?.text ?? ''}
+          />
+        ) : (
+          <Link to={value.languages[translation.language]?.href ?? ''} key={translation.language}>
+            <EditableElement
+              element="span"
+              label={`${capitalize(translation.language)} label`}
+              onValueChange={(text) => {
+                setValue({
+                  languages: {
+                    ...value.languages,
+                    [translation.language]: { ...value.languages[translation.language], text },
+                  },
+                })
+              }}
+              value={value.languages[translation.language]?.text ?? ''}
+              style={styles.otherLanguage}
+            />
+          </Link>
         ),
       )}
     </Row>
   )
+}
+
+const styles = {
+  outerCell: {
+    ...StyleDefaults.inline.colors,
+    ...Text.body.main.regular,
+    backgroundColor: '#3a3a3a',
+    color: Colors.white,
+    fontSize: 12,
+    padding: `${Spacing.size.tiny}px 0`,
+  } as CSSProperties,
+  currentLanguage: {
+    fontWeight: Font.weight.bold,
+    marginRight: 12,
+  } as CSSProperties,
+  otherLanguage: {
+    textDecoration: 'underline',
+    marginRight: 12,
+    color: Colors.white,
+  } as CSSProperties,
 }
