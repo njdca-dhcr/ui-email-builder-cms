ce903180b78ce2f32905323d855e1c9ed450845e
Michael Crismali <michael@crismali.com>
2025-01-19 15:21:45 -0600

    reorder imports to prevent css warning

--- src/pages/settings/users.tsx
+++ src/pages/settings/users.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react'
 import { HeadFC } from 'gatsby'
 import { List, ListItem, LoadingOverlay } from 'src/ui'
+import { DestroyUser } from 'src/ui/DestroyDialog'
 import { Layout, PageContent, Sidebar } from 'src/ui/Settings/Shared'
 import { formatPageTitle } from 'src/utils/formatPageTitle'
 import { useUsers } from 'src/network/users'
 import { useRedirectIfNotSignedIn } from 'src/utils/useRedirectIfNotSignedIn'
-import './users.css'
-import { DestroyUser } from 'src/ui/DestroyDialog'
 import { EditUserDialog } from 'src/ui/EditDialog/EditUserDialog'
 import { useRedirectIfNotAdmin } from 'src/utils/useRedirectIfNotAdmin'
+import './users.css'
 
 const UsersPage: FC = () => {
   useRedirectIfNotSignedIn()
