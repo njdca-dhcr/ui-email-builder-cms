1c348fe590cc939efb1cabe3acd28c4ad6ef8ba2
Eryan Cobham <eryan@cobwebsconsulting.com>
2025-01-29 15:59:50 -0600

    added faker

--- src/pages/__tests__/dashboard.test.tsx
+++ src/pages/__tests__/dashboard.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { faker } from '@faker-js/faker'
 import DashboardPage from '../dashboard'
 import {
   EmailTemplateIndex,
